e0f2fa62538a263feca7aa501bb358fea219fc7a
Maksym Panov <work.maksym.panov@gmail.com>
2024-05-06 13:23:24 +0300

    Prepared docker-compose.yml

--- src/main/java/com/maksympanov/hneu/mjt/sbcrud/sbcrud/CRUDApplication.java
+++ src/main/java/com/maksympanov/hneu/mjt/sbcrud/sbcrud/CRUDApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbcrudApplication {
+public class CRUDApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SbcrudApplication.class, args);
+        SpringApplication.run(CRUDApplication.class, args);
     }
 
 }
