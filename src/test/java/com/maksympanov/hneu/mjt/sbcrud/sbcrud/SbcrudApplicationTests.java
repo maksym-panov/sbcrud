aa0a3f000b57ec33d39e12287bc43f926a011e0e
Maksym Panov <maksym.panov@hneu.net>
2024-05-06 11:52:51 +0300

    Init project

---
+++ src/test/java/com/maksympanov/hneu/mjt/sbcrud/sbcrud/SbcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksympanov.hneu.mjt.sbcrud.sbcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
